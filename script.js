ee74a7616c0662daff7d49c3d993359965561790
Ferlo94 <laferlart@gmail.com>
2022-05-04 11:34:23 +0200

    snack1

---
+++ script.js
@@ -0,0 +1,11 @@
+const firstNumber = prompt("inserisci un numero")
+const secondNumber = prompt("inserisci un numero")
+
+if(firstNumber > secondNumber) {
+    console.log(firstNumber)
+}
+
+if(secondNumber>firstNumber) {
+    console.log(secondNumber)
+}
+
