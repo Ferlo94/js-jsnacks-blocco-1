5c24082c6e35f20d9074e966ee93474665cf0c24
Ferlo94 <laferlart@gmail.com>
2022-05-04 12:29:40 +0200

    corretto

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,12 +1,22 @@
-const  numberOne = Number(prompt("inserisci un numero"));
-const  numberTwo = Number(prompt("inserisci un numero"));
-const  numberThree = Number(prompt("inserisci un numero"));
-const  numberFour = Number(prompt("inserisci un numero"));
-const  numberFive = Number(prompt("inserisci un numero"));
-const  numberSix = Number(prompt("inserisci un numero"));
-const  numberSeven = Number(prompt("inserisci un numero"));
-const  numberEight = Number(prompt("inserisci un numero"));
-const  numberNine = Number(prompt("inserisci un numero"));
-const  numberTen = Number(prompt("inserisci un numero"));
+// const  numberOne = Number(prompt("inserisci un numero"));
+// const  numberTwo = Number(prompt("inserisci un numero"));
+// const  numberThree = Number(prompt("inserisci un numero"));
+// const  numberFour = Number(prompt("inserisci un numero"));
+// const  numberFive = Number(prompt("inserisci un numero"));
+// const  numberSix = Number(prompt("inserisci un numero"));
+// const  numberSeven = Number(prompt("inserisci un numero"));
+// const  numberEight = Number(prompt("inserisci un numero"));
+// const  numberNine = Number(prompt("inserisci un numero"));
+// const  numberTen = Number(prompt("inserisci un numero"));
 
-console.log(numberOne + numberTwo + numberThree + numberFour + numberFive + numberSix + numberSeven + numberEight + numberNine + numberTen)
+// console.log(numberOne + numberTwo + numberThree + numberFour + numberFive + numberSix + numberSeven + numberEight + numberNine + numberTen)
+
+let total = 0;
+
+for (let i = 0; i < 10; i++) {
+    const number = Number(prompt("inserisci un numero"));
+    total += number;
+
+}
+
+console.log(total);
