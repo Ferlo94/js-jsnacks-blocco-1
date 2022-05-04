3d8dc25052a19dbe61ca489ea37c031ef64b5100
Ferlo94 <laferlart@gmail.com>
2022-05-04 12:06:30 +0200

    snack-3

---
+++ snack-3/script.js
@@ -0,0 +1,12 @@
+const  numberOne = Number(prompt("inserisci un numero"));
+const  numberTwo = Number(prompt("inserisci un numero"));
+const  numberThree = Number(prompt("inserisci un numero"));
+const  numberFour = Number(prompt("inserisci un numero"));
+const  numberFive = Number(prompt("inserisci un numero"));
+const  numberSix = Number(prompt("inserisci un numero"));
+const  numberSeven = Number(prompt("inserisci un numero"));
+const  numberEight = Number(prompt("inserisci un numero"));
+const  numberNine = Number(prompt("inserisci un numero"));
+const  numberTen = Number(prompt("inserisci un numero"));
+
+console.log(numberOne + numberTwo + numberThree + numberFour + numberFive + numberSix + numberSeven + numberEight + numberNine + numberTen)
