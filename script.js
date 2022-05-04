6eff02c899d87ed66aff7a02785ff4c2a38b1bf2
Ferlo94 <laferlart@gmail.com>
2022-05-04 11:38:42 +0200

    messaggio console

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const firstNumber = prompt("inserisci un numero")
-const secondNumber = prompt("inserisci un numero")
+const firstNumber = prompt("inserisci un numero");
+const secondNumber = prompt("inserisci un numero");
 
 if(firstNumber > secondNumber) {
-    console.log(firstNumber)
+    console.log(firstNumber, "IL PRIMO NUMERO e' MAGGIORE")
 }
 
-if(secondNumber>firstNumber) {
-    console.log(secondNumber)
+if(secondNumber > firstNumber) {
+    console.log(secondNumber, "IL SECONDO NUMERO e' MAGGIORE")
 }
 
